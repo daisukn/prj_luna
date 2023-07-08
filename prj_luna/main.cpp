859cce40a378430072252641b2793fc746b26992
Daisuke <daisukn@outlook.com>
2023-07-09 03:42:17 +0900

    Initial Commit

---
+++ prj_luna/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  prj_luna
+//
+//  Created by Daisuke Nishimori on 2023/07/09.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
